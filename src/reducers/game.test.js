bc1576450369361d49fcafbaaa1b40a6f5dcf56c
NULL-OPERATOR <NULL-OPERATOR@users.noreply.github.com>
2017-11-05 15:04:23 +0000

    add more tests

--- src/reducers/game.test.js
+++ src/reducers/game.test.js
@@ -42,6 +42,79 @@ describe('game reducer', () => {
     expect(game(initialState, {type: 'INCREASE_SCORE', id: 1})).toEqual(expectedState)
   })
 
+  it('can win the game at 40-Love', () => {
+    const customInitialState = {
+      winner: '',
+      players: [
+        {
+          id: 1,
+          score: 3,
+          scoreText: '40',
+          name: 'Player 1'
+        }, {
+          id: 2,
+          score: 0,
+          scoreText: 'Love',
+          name: 'Player 2'
+        }
+      ]
+    }
+    const expectedState = {
+      winner: 'Player 1',
+      players: [
+        {
+          id: 1,
+          score: 3,
+          scoreText: 'Game',
+          name: 'Player 1'
+        }, {
+          id: 2,
+          score: 0,
+          scoreText: 'Love',
+          name: 'Player 2'
+        }
+      ]
+    }
+    expect(game(customInitialState, {type: 'INCREASE_SCORE', id: 1})).toEqual(expectedState)
+  })
+
+
+  it('changes to 40-40, from 40-Advantage when player 1 scores', () => {
+    const customInitialState = {
+      winner: '',
+      players: [
+        {
+          id: 1,
+          score: 3,
+          scoreText: '40',
+          name: 'Player 1'
+        }, {
+          id: 2,
+          score: 4,
+          scoreText: 'Advantage',
+          name: 'Player 2'
+        }
+      ]
+    }
+    const expectedState = {
+      winner: '',
+      players: [
+        {
+          id: 1,
+          score: 3,
+          scoreText: '40',
+          name: 'Player 1'
+        }, {
+          id: 2,
+          score: 3,
+          scoreText: '40',
+          name: 'Player 2'
+        }
+      ]
+    }
+    expect(game(customInitialState, {type: 'INCREASE_SCORE', id: 1})).toEqual(expectedState)
+  })
+
 
   it('should handle RESET_GAME action', () => {
     const inGameState = {
